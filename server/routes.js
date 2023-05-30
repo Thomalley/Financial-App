3890a8e30a5e1ce5396243de616e6ddafa8c6f13
Nahuel Paolinelli <nahuel.paolinelli@gmail.com>
2023-05-30 11:52:49 -0300

    controladores y rutas completadas según task y back

--- server/routes.js
+++ server/routes.js
@@ -10,18 +10,14 @@ const userCtrl = require('./controllers/user');
 
 // Auth
 router.post('/auth/login', authCtrl.postLogin);
-router.post('/auth/register', authCtrl.postRegister);
+router.post('/user/create', authCtrl.postRegister);
 router.post('/auth/recover-password', authCtrl.recoverPassword);
 router.post('/auth/reset-password', authCtrl.resetPassword);
-router.post('/auth/user-verification', authCtrl.userVerification);
 router.get('/auth/users/:id', authCtrl.getUserById);
+// router.post('/auth/user-verification', authCtrl.userVerification);
 
 // User
-router.get('/admin/users', userCtrl.getUsersPerPage);
-router.get('/all/users', userCtrl.getAllUsers);
-router.post('/user/create', userCtrl.postCreateUser);
 router.put('/user/edit', userCtrl.putUserById);
 router.delete('/user/:id', userCtrl.deleteUser);
-router.get('/users/role/:roleId', userCtrl.getUsersByRole);
 
 module.exports = router;
